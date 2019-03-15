599bb6f8cc07f63aadd9b5daf301a6a641024808
NitroSkate <00159817@uca.edu.sv>
2019-03-15 15:28:25 -0600

    Primer Commit

---
+++ app/src/main/java/com/ejemplo/pdm_labo2_00159817_ej2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ejemplo.pdm_labo2_00159817_ej2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}

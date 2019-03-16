493386ec5d6c2715aa2d522708b2ea99dc43c0d3
NitroSkate <00159817@uca.edu.sv>
2019-03-15 19:24:56 -0600

    Terminado

--- app/src/main/java/com/ejemplo/pdm_labo2_00159817_ej2/MainActivity.java
+++ app/src/main/java/com/ejemplo/pdm_labo2_00159817_ej2/MainActivity.java
@@ -2,14 +2,20 @@ package com.ejemplo.pdm_labo2_00159817_ej2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn1;
     Button btn2;
     Button btn3;
+    ArrayList <Integer> lister = new ArrayList<Integer>();
+    ArrayList <Integer> listeg = new ArrayList<Integer>();
+    ArrayList <Integer> listeb = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        int viewId = v.getId();
+        Log.i("id",viewId + "");
+        switch (viewId){
+            case R.id.btn1:
+                agregarR();
+                btn1.setBackgroundColor(getResources().getColor(lister.get((int)(Math.random()*6))));
+                break;
+            case R.id.btn2:
+                agregarG();
+                btn2.setBackgroundColor(getResources().getColor(listeg.get((int)(Math.random()*6))));
+                break;
+            case R.id.btn3:
+                agregarB();
+                btn3.setBackgroundColor(getResources().getColor(listeb.get((int)(Math.random()*6))));
+                break;
+        }
+    }
 
+    public void agregarR(){
+        lister.add(R.color.red);
+        lister.add(R.color.red1);
+        lister.add(R.color.red2);
+        lister.add(R.color.red3);
+        lister.add(R.color.red4);
+        lister.add(R.color.red5);
+    }
+    public void agregarG(){
+        listeg.add(R.color.green);
+        listeg.add(R.color.green1);
+        listeg.add(R.color.green2);
+        listeg.add(R.color.green3);
+        listeg.add(R.color.green4);
+        listeg.add(R.color.green5);
+    }
+    public void agregarB(){
+        listeb.add(R.color.blue);
+        listeb.add(R.color.blue1);
+        listeb.add(R.color.blue2);
+        listeb.add(R.color.blue3);
+        listeb.add(R.color.blue4);
+        listeb.add(R.color.blue5);
     }
 }
